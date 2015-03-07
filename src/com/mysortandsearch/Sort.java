e7813cd08d4b15bbedd24e23380aacb380218456
Julien Simon <julien@julien.org>
2015-03-07 20:28:03 +0100

    Add bubble sort. Add method to check if a list is sorted

--- src/com/mysortandsearch/Sort.java
+++ src/com/mysortandsearch/Sort.java
@@ -4,6 +4,31 @@ import java.util.List;
 
 public class Sort<T> {
 
+	//
+	// Bubble sort
+	// Best case o(n), median case o(n^2), worst case o(n^2)
+	//
+	public static <T> void bubbleSort(List<T> list) {
+		boolean swap;
+		do {
+			swap = false;
+			for (int i=0;i<list.size()-1;i++) {
+				T left = list.get(i);
+				T right = list.get(i+1);
+				if ( ((Comparable)left).compareTo(right) > 0 ) {
+					list.set(i, right);
+					list.set(i+1,  left);
+					swap = true;
+				}
+			}
+		} while (swap);
+	}
+	
+	//
+	// Insertion sort
+	// Best case o(n), median case o(n^2), worst case o(n^2)
+	//
+	
 	public static <T> void insertionSort(List<T> list) {
 		for (int i=1;i<list.size();i++) {
 			insert(list, i, list.get(i));
@@ -13,11 +38,25 @@ public class Sort<T> {
 	private static <T> void insert(List<T> list, int index, T element) {
 		int position = index-1;
 		// For each element, start from its original position, move larger neighbors to the right
-		while ( (position>=0) && ( ((Comparable<T>)list.get(position)).compareTo(element) > 0) ) {
+		while ( (position>=0) && ( ((Comparable)list.get(position)).compareTo(element) > 0) ) {
 			list.set(position+1, list.get(position));
 			position--;
 		}
 		// Put the element to the right of its first smaller neighbor
 		list.set(position+1, element);
+	}
+	
+	//
+	// Utility method to check if a list is sorted
+	//
+	public static <T> boolean isSorted(List<T> list) {
+		for (int i=0;i<list.size()-1;i++) {
+			T left = list.get(i);
+			T right = list.get(i+1);
+			if ( ((Comparable)left).compareTo(right) > 0 ) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
--- src/com/mysortandsearch/TestSort.java
+++ src/com/mysortandsearch/TestSort.java
@@ -21,9 +21,16 @@ public class TestSort {
 	public void testInsertion() {
 		list = lg.build(10, 100);
 		Sort.insertionSort(list);
-		for (int i=0;i<list.size()-1;i++) {
-			assertTrue(list.get(i) <= list.get(i+1));
-		}
+		assertTrue(Sort.isSorted(list));
+		list = null;
+	}
+	
+	@Test
+	public void testBubble() {
+		list = lg.build(10, 100);
+		Sort.bubbleSort(list);
+		assertTrue(Sort.isSorted(list));
+
 		list = null;
 	}
 }
