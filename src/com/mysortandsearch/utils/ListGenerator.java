7ea41037719a48aa556857999c82303f6945e96d
Julien Simon <julien@julien.org>
2015-03-13 16:30:16 +0100

    Add method to generate list of unique integers

--- src/com/mysortandsearch/utils/ListGenerator.java
+++ src/com/mysortandsearch/utils/ListGenerator.java
@@ -18,16 +18,24 @@ public class ListGenerator {
 		}
 		return list;
 	}
-
-	public ArrayList<MyInteger> buildMyIntegerList(int count, int maxValue) {
-		ArrayList<MyInteger> list = new ArrayList<MyInteger>();
-		for (int i = 0; i < count; i++) {
-			list.add(new MyInteger(rand.nextInt(maxValue + 1)));
+	
+	public ArrayList<Integer> buildUnique(int count, int maxValue) {
+		if (maxValue < count) {
+			return null;
+		}
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		int counter = 0;
+		while (counter != count) {
+			int value = rand.nextInt(maxValue + 1);
+			if (!list.contains(value)) {
+				list.add(value);
+				counter++;
+			}
 		}
 		return list;
 	}
 	
-	public ArrayList<HasAnIntegerKey> buildMyIntegerList2(int count, int maxValue) {
+	public ArrayList<HasAnIntegerKey> buildMyIntegerList(int count, int maxValue) {
 		ArrayList<HasAnIntegerKey> list = new ArrayList<HasAnIntegerKey>();
 		for (int i = 0; i < count; i++) {
 			list.add(new MyInteger(rand.nextInt(maxValue + 1)));
